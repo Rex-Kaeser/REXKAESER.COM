b1a0ba02997c2f453306ca87a1b7afc3d654eeb7
RexGameStudio <101283720+RexGameStudio@users.noreply.github.com>
2024-10-23 15:01:04 -0600

    Added url tags for timeline popups to allow direct linking

--- public/js/popupControl.js
+++ public/js/popupControl.js
@@ -23,7 +23,29 @@ function LoadInclude(node, path){
 }
 
 function closePopup(){
-    document.getElementById('popup-div').classList.add('hidden');
+    //document.getElementById('popup-div').classList.add('hidden');
+    setPopup("");
+}
+
+function updateHashPopup(){
+    var hash = window.location.hash.replace("#", "");
+    if (hash.length < 1){
+        document.getElementById('popup-div').classList.add('hidden');
+    }
+    var path = "./public/popups/" + hash + ".txt";
+    fetch(path).then(r => r.text()).then(content => {
+        let lines = content.split('\n');
+        if (lines.length == 2){
+            var name = lines[0]
+            var path = lines[1];
+            console.log(lines);
+            openPopup(name, path);
+        }
+    });
+}
+
+function setPopup(hash){
+    window.location.hash = hash;
 }
 
 function openPopup(name, path){
@@ -32,3 +54,10 @@ function openPopup(name, path){
     document.getElementById('popup-content').innerHTML = "Loading..."
     LoadInclude(document.getElementById('popup-content'), path);
 }
+
+window.addEventListener("hashchange", function() {
+    updateHashPopup();
+});  
+
+updateHashPopup();
+
